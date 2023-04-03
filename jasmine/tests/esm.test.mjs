aca72e481e72df17ea41cd8cd8d83129379fffc2
Jason Bedard <jason@aspect.dev>
2023-04-03 16:41:54 -0700

    test: add various test cases from rules_nodejs (#49)
    Fixes https://github.com/aspect-build/rules_jasmine/issues/8

---
+++ jasmine/tests/esm.test.mjs
@@ -0,0 +1,5 @@
+describe('spec in file ending with *.mjs', () => {
+    it('should run', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ jasmine/tests/failing.test.js
@@ -0,0 +1,8 @@
+describe('test failures', () => {
+  it('should fail', () => {
+    expect(false).toBeTruthy();
+  });
+  it('should fail', () => {
+    expect(false).toBeTruthy();
+  });
+});
